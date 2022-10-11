71aba97605fd39125a15682595e02b0a195317c5
gadogado092 <gadogado092>
2022-10-11 09:41:58 +0800

    finish birthday

--- app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
@@ -3,15 +3,19 @@ package com.example.juaraandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.juaraandroidcompose.ui.theme.JuaraAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.Blue
                 ) {
-                    Greeting("Android gan")
+//                    Greeting("Android gan")
                 }
             }
         }
@@ -32,16 +36,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = Color.Magenta) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+fun Greeting(to: String, from: String) {
+    Column {
+        Text(
+            text = "Hello $to!",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.Start)
+                .padding(start = 16.dp, top = 16.dp)
+        )
+        Text(
+            text = "Dari $from!", fontSize = 24.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+                .padding(start = 16.dp, end = 16.dp)
+        )
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun GreetingWithImage(to: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image, contentDescription = null, modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        Greeting(to = to, from = from)
+    }
+}
+
+@Preview(name = "my_preview", showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     JuaraAndroidComposeTheme {
-        Greeting("Android Juara")
+        GreetingWithImage("Android Juara", "Makassar")
     }
 }
