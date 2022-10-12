e83d7ba32b8be418da3d159c1db4ec61b0fe1c9b
gadogado092 <gadogado092>
2022-10-12 11:43:45 +0800

    init UI dice rool

--- app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
@@ -5,17 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.juaraandroidcompose.ui.theme.JuaraAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,57 +21,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JuaraAndroidComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Blue
-                ) {
-//                    Greeting("Android gan")
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(to: String, from: String) {
-    Column {
-        Text(
-            text = "Hello $to!",
-            fontSize = 36.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.Start)
-                .padding(start = 16.dp, top = 16.dp)
-        )
-        Text(
-            text = "Dari $from!", fontSize = 24.sp,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentWidth(Alignment.End)
-                .padding(start = 16.dp, end = 16.dp)
-        )
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = R.drawable.dice_1), contentDescription = "1")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(R.string.rool))
+        }
     }
 }
 
+@Preview(name = "default_preview", showSystemUi = true)
 @Composable
-fun GreetingWithImage(to: String, from: String) {
-    val image = painterResource(id = R.drawable.androidparty)
-    Box {
-        Image(
-            painter = image, contentDescription = null, modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
-        Greeting(to = to, from = from)
-    }
-}
-
-@Preview(name = "my_preview", showSystemUi = true)
-@Composable
-fun DefaultPreview() {
+fun DiceRollerApp() {
     JuaraAndroidComposeTheme {
-        GreetingWithImage("Android Juara", "Makassar")
+        DiceWithButtonAndImage(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
     }
 }
