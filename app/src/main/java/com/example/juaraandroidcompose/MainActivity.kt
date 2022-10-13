074f1e5a7b8779aebaf755f39079eb3287aad6df
gadogado092 <gadogado092>
2022-10-13 12:16:13 +0800

    init for material app

--- app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
@@ -3,82 +3,112 @@ package com.example.juaraandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.juaraandroidcompose.data.Datasource
-import com.example.juaraandroidcompose.model.Affirmation
+import com.example.juaraandroidcompose.data.Dog
+import com.example.juaraandroidcompose.data.dogs
 import com.example.juaraandroidcompose.ui.theme.JuaraAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AffirmationApp()
+            JuaraAndroidComposeTheme {
+                WoofApp()
+            }
         }
     }
 }
 
+/**
+ * Composable that displays an app bar and a list of dogs.
+ */
 @Composable
-fun AffirmationApp() {
-    val context = LocalContext.current
-    JuaraAndroidComposeTheme {
-        Scaffold(
-            content = {
-                AffirmationList(affirmationList = Datasource().loadAffirmations())
-            }
+fun WoofApp() {
+    LazyColumn {
+        items(dogs) {
+            DogItem(dog = it)
+        }
+    }
+}
+
+/**
+ * Composable that displays a list item containing a dog icon and their information.
+ *
+ * @param dog contains the data that populates the list item
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        DogIcon(dog.imageResourceId)
+        DogInformation(dog.name, dog.age)
+    }
+}
+
+/**
+ * Composable that displays a photo of a dog.
+ *
+ * @param dogIcon is the resource ID for the image of the dog
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier
+            .size(64.dp)
+            .padding(8.dp),
+        painter = painterResource(dogIcon),
+        /*
+         * Content Description is not needed here - image is decorative, and setting a null content
+         * description allows accessibility services to skip this element during navigation.
+         */
+        contentDescription = null
+    )
+}
+
+/**
+ * Composable that displays a dog's name and age.
+ *
+ * @param dogName is the resource ID for the string of the dog's name
+ * @param dogAge is the Int that represents the dog's age
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = stringResource(dogName),
+            modifier = modifier.padding(top = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.years_old, dogAge)
         )
     }
 }
 
-@Composable
-fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
-    Card(modifier = modifier.padding(8.dp), elevation = 4.dp) {
-        Column {
-            Image(
-                painter = painterResource(affirmation.imageResourceId),
-                contentDescription = stringResource(affirmation.stringResourceId),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(194.dp),
-                contentScale = ContentScale.Crop
-            )
-            Text(
-                text = stringResource(affirmation.stringResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.h6
-            )
-        }
-    }
-}
-
-@Composable
-private fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
-    LazyColumn {
-        items(affirmationList) { affirmation ->
-            AffirmationCard(affirmation)
-        }
-    }
-}
-
+/**
+ * Composable that displays what the UI of the app looks like in light theme in the design tab.
+ */
 @Preview
 @Composable
-private fun AffirmationCardPreview() {
-    AffirmationApp()
+fun WoofPreview() {
+    JuaraAndroidComposeTheme(darkTheme = false) {
+        WoofApp()
+    }
 }
