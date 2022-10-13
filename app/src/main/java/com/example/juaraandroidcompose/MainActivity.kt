330d5edec4c3baf32c7a599af70437dd195dd894
gadogado092 <gadogado092>
2022-10-13 09:39:36 +0800

    init ui calculate tip

--- app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
+++ app/src/main/java/com/example/juaraandroidcompose/MainActivity.kt
@@ -3,17 +3,17 @@ package com.example.juaraandroidcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.juaraandroidcompose.ui.theme.JuaraAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,40 +21,42 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JuaraAndroidComposeTheme {
-                DiceRollerApp()
+                Surface() {
+                    TipTimeScreen()
+                }
             }
         }
     }
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = imageResource), contentDescription = result.toString())
+fun TipTimeScreen() {
+    Column(
+        modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(text = stringResource(R.string.rool))
-        }
+        EditNumberField()
     }
 }
 
-@Preview(name = "default_preview", showSystemUi = true)
 @Composable
-fun DiceRollerApp() {
+fun EditNumberField() {
+    TextField(
+        value = "",
+        onValueChange = {}
+    )
+}
+
+@Preview(name = "default_preview", showSystemUi = false, showBackground = true)
+@Composable
+fun DefaultPreview() {
     JuaraAndroidComposeTheme {
-        DiceWithButtonAndImage(
-            modifier = Modifier
-                .fillMaxSize()
-                .wrapContentSize(Alignment.Center)
-        )
+        TipTimeScreen()
     }
 }
